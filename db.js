ea5be0101c9f7394908868f4c13b05237aeb58f0
smart-buy-advisor <kiseof@gmail.com>
2025-06-22 15:28:17 +0300

    ver 1.2 db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306,
+    host: 'mysql://root:zTOFACqJnxvJTxnoNgVbWdHcRJuFtsPg@metro.proxy.rlwy.net:31319/railway',
+    user: 'root',
+    password: 'zTOFACqJnxvJTxnoNgVbWdHcRJuFtsPg',
+    database: 'railway',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
